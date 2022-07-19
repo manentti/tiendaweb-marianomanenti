8c21b171e76f8af191dbfbcf98332530bcd58b5d
mariano <marianoagustinmanentti@gmail.com>
2022-07-18 22:14:56 -0300

    agregando un navbar

---
+++ reart/tarea-1/app41080/src/components/navbar/navbar.js
@@ -0,0 +1,19 @@
+
+
+const navbar = () => {
+    return (
+        <nav>
+            <div>
+                comercio
+            </div>
+            <div>
+                <butom>peliculas</butom>
+                <butom>series</butom>
+                <butom>juegos</butom>
+            </div>
+        </nav>
+
+    )
+};
+
+export default navbar;
